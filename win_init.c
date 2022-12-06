9dcb0b3fc1f6ae565cd534fadee6dd7b1b9907d6
amaraoussama94 <wassa36@yahoo.fr>
2022-12-06 17:08:44 +0100

    adding win_init for windows system

---
+++ win_init.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main() 
+{
+    WSADATA d;
+    /*function is called with the requested version, Winsock 2.2 in this case*/
+    if (WSAStartup(MAKEWORD(2, 2), &d)) 
+    {
+        printf("Failed to initialize.\n");
+        return -1;
+    }
+    /*Winsock program is finished*/
+    WSACleanup();
+    printf("Ok.\n");
+    return 0;
+}
