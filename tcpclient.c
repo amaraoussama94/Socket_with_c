3efd835bbe55104753447296ce1fd033c35a44cc
amaraoussama94 <wassa36@yahoo.fr>
2022-12-09 08:54:39 +0100

    adding some comment to tcp client  , continue with the code

--- tcpclient.c
+++ tcpclient.c
@@ -1,3 +1,6 @@
+/************************************************/
+/** arg 1 @IP      arg 2 Port number           **/
+/***********************************************/
 #include "macrosheaderwx.h"
 
 #if defined(_WIN32)
@@ -30,9 +33,19 @@ int main(int argc, char *argv[])
     struct addrinfo hints;
     // clearing the struct instance  alll zero
     memset(&hints, 0, sizeof(hints));
-    hints.ai_socktype = SOCK_STREAM;//IP4
+    //TCP  else use SOCK_DGRAM for UDP
+    hints.ai_socktype = SOCK_STREAM;
     struct addrinfo *peer_address;
-    if (getaddrinfo(argv[1], argv[2], &hints, &peer_address)) {
-    fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
-    return 1;
+    /*We don't need to set the family here,
+     as we can let getaddrinfo() decide if
+      IPv4 or IPv6 is the proper protocol to use*/
+    if (getaddrinfo(argv[1], argv[2], &hints, &peer_address)) 
+    {
+        fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
     }
+    printf("Remote address is: ");
+    char address_buffer[100];
+    char service_buffer[100];
+    getnameinfo(peer_address->ai_addr, peer_address->ai_addrlen,address_buffer, sizeof(address_buffer),service_buffer, sizeof(service_buffer),NI_NUMERICHOST);
+    printf("%s %s\n", address_buffer, service_buffer);
