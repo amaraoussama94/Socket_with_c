22a0d8929cf3d2acb90975167d4787d8d2184196
amaraoussama94 <wassa36@yahoo.fr>
2022-12-09 11:53:42 +0100

    all code is doen wait for test

--- tcpserver.c
+++ tcpserver.c
@@ -78,27 +78,80 @@ int main()
     SOCKET max_socket = socket_listen; 
 
     printf("Waiting for connections...\n");
-    while(1) {
-    fd_set reads;
-    //If we didn't copy master, we would lose its data.
-    reads = master;
-    /*timeout value of 0 (NULL) to select() so that it doesn't return until a socket in
-    the master set is ready to be read from. At the beginning of our program, master only
-    contains socket_listen, but as our program runs, we add each new connection to
-    master.*/
-    if (select(max_socket+1, &reads, 0, 0, 0) < 0) 
+    while(1) 
     {
-        fprintf(stderr, "select() failed. (%d)\n", GETSOCKETERRNO());
-        return 1;
-    }
-    //If a socket, X, was flagged by select() ready to be read , then FD_ISSET(X, &reads) is true
-    SOCKET i;
-    for(i = 1; i <= max_socket; ++i) 
-    {
-        if (FD_ISSET(i, &reads)) 
+        fd_set reads;
+        //If we didn't copy master, we would lose its data.
+        reads = master;
+        /*timeout value of 0 (NULL) to select() so that it doesn't return until a socket in
+        the master set is ready to be read from. At the beginning of our program, master only
+        contains socket_listen, but as our program runs, we add each new connection to
+        master.*/
+        if (select(max_socket+1, &reads, 0, 0, 0) < 0) 
         {
-
-        //We should first determine whether the current socket 
-        //is the listening one or not. If it is, we call accept()
+            fprintf(stderr, "select() failed. (%d)\n", GETSOCKETERRNO());
+            return 1;
         }
-    }
+        //If a socket, X, was flagged by select() ready to be read , then FD_ISSET(X, &reads) is true
+        SOCKET i;
+        for(i = 1; i <= max_socket; ++i) 
+        {
+            if (FD_ISSET(i, &reads)) 
+            {
+                //If the socket is socket_listen, then we accept() the connection
+                if (i == socket_listen) 
+                {
+                    struct sockaddr_storage client_address;
+                    socklen_t client_len = sizeof(client_address);
+                    SOCKET socket_client = accept(socket_listen,(struct sockaddr*) &client_address,&client_len);
+                    if (!ISVALIDSOCKET(socket_client)) 
+                    {
+                        fprintf(stderr, "accept() failed. (%d)\n",
+                        GETSOCKETERRNO());
+                        return 1;
+                    }
+                    //for child things  set the new client to listen for 
+                    FD_SET(socket_client, &master);
+                    if (socket_client > max_socket)
+                        max_socket = socket_client;
+                    char address_buffer[100];
+                    // get the information of the client 
+                    getnameinfo((struct sockaddr*)&client_address,client_len,address_buffer, sizeof(address_buffer), 0, 0,NI_NUMERICHOST);
+                    printf("New connection from %s\n", address_buffer);
+                } 
+                //If the socket i is not socket_listen
+                //In this case, we need to read it with recv(), convert it into uppercase using the
+                //built-in toupper() function, and send the data back:
+                else 
+                {
+                    char read[1024];
+                    //get the recv msg into the baffer
+                    int bytes_received = recv(i, read, 1024, 0);
+                    //check for error 
+                    if (bytes_received < 1) 
+                    {
+                        FD_CLR(i, &master);
+                        CLOSESOCKET(i);
+                        continue;
+                    }
+                    int j;
+                    //make it all upper case
+                    for (j = 0; j < bytes_received; ++j)
+                        read[j] = toupper(read[j]);
+                        //send it to the client 
+                    send(i, read, bytes_received, 0);
+                }
+            }//if FD_ISSET
+        }//for i to max_socket
+    } //while(1)
+
+    //If the client has disconnected, then recv() returns a non-positive number. In this case, we
+    //remove that socket from the master socket set, and we also call CLOSESOCKET() on it to clean up
+    printf("Closing listening socket...\n");
+    CLOSESOCKET(socket_listen);
+    #if defined(_WIN32)
+        WSACleanup();
+    #endif
+    printf("Finished.\n");
+    return 0;
+}
