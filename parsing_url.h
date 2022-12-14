239f25105c82b84c499a186e3d06c52c20506b3d
amaraoussama94 <wassa36@yahoo.fr>
2022-12-14 10:37:03 +0100

    apply best practise for headre file

--- parsing_url.h
+++ parsing_url.h
@@ -1,2 +1,12 @@
+#ifndef PARSING_URL_H
+#define PARSING_URL_H  1
+
 #include "macrosheaderwx.h" 
+
+__BEGIN_DECLS
+
 void parse_url(char *url, char **hostname, char **port, char** path) ;
+
+__END_DECLS
+
+#endif /* PARSING_URL.h */
