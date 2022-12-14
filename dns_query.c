7406e59bab9c96408b91976dc10e87dc3e345f78
amaraoussama94 <wassa36@yahoo.fr>
2022-12-14 08:28:47 +0100

    cont withdns qeury , add sending query aprt

--- dns_query.c
+++ dns_query.c
@@ -226,7 +226,7 @@ void print_dns_message(const char *message, int msg_length)
             else if (rdlen == 16 && type == 28)
             { /* AAAA Record */
                 printf("Address ");
-                int j;
+                int j;/***********************************************2 nd commit  start here ***********************************************/
                 for (j = 0; j < rdlen; j+=2) 
                 {
                     printf("%02x%02x", p[j], p[j+1]);
@@ -253,3 +253,140 @@ void print_dns_message(const char *message, int msg_length)
     }
     printf("\n");
 }
+
+/*********************
+ * Sending the query *
+*********************/
+
+int main(int argc, char *argv[]) 
+{
+    //checks that the user passed in a hostname and record type to query
+    if (argc < 3) 
+    {
+        printf("Usage:\n\tdns_query hostname type\n");
+        printf("Example:\n\tdns_query example.com aaaa\n");
+        exit(0);
+    }
+    //checks that the hostname isn't more than 255 characters long. Hostnames longer than that aren't allowed by the DNS standard,
+    if (strlen(argv[1]) > 255) 
+    {
+        fprintf(stderr, "Hostname too long.");
+        exit(1);
+    }
+    //interpret the record type requested by the user
+    unsigned char type;
+    if (strcmp(argv[2], "a") == 0) 
+    {
+        type = 1;
+    } 
+    else if (strcmp(argv[2], "mx") == 0) 
+    {
+        type = 15;
+    } 
+    else if (strcmp(argv[2], "txt") == 0) 
+    {
+        type = 16;
+    } 
+    else if (strcmp(argv[2], "aaaa") == 0) 
+    {
+        type = 28;
+    } 
+    else if (strcmp(argv[2], "any") == 0) 
+    {
+        type = 255;
+    } 
+    else 
+    {
+        fprintf(stderr, "Unknown type '%s'. Use a, aaaa, txt, mx, or any.",argv[2]);
+        exit(1);
+    }
+    //initialize Winsock
+        #if defined(_WIN32)
+        WSADATA d;
+        if (WSAStartup(MAKEWORD(2, 2), &d)) 
+        {
+            fprintf(stderr, "Failed to initialize.\n");
+            return 1;
+        }
+    #endif
+    /*Our program connects to 8.8.8.8, which is a public DNS server run by Google. */
+    //we are connecting on UDP port 53
+    printf("Configuring remote address...\n");
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_socktype = SOCK_DGRAM;
+    struct addrinfo *peer_address;
+    if (getaddrinfo("8.8.8.8", "53", &hints, &peer_address)) 
+    {
+        fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+    printf("Creating socket...\n");
+    SOCKET socket_peer;
+    socket_peer = socket(peer_address->ai_family,peer_address->ai_socktype, peer_address->ai_protocol);
+    if (!ISVALIDSOCKET(socket_peer)) 
+    {
+        fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+    //constructs the data for the DNS query message. The first 12 bytes compose the header and are known at compile time
+    //sets our query's ID to 0xABCD, sets a recursion request, and indicates that we are attaching 1 question
+    char query[1024] = {
+                        0xAB, 0xCD, /* ID */
+                        0x01, 0x00, /* Set recursion */
+                        0x00, 0x01, /* QDCOUNT */
+                        0x00, 0x00, /* ANCOUNT */
+                        0x00, 0x00, /* NSCOUNT */
+                        0x00, 0x00 /* ARCOUNT */
+                        };
+    //encode the user's desired hostname into the query
+
+    //sets a new pointer, p, to the end of the query header
+    char *p = query + 12;
+    //ptr h which we use to loop through the hostname
+    char *h = argv[1];
+    //*h is equal to zero when we've finished reading the hostname
+    while(*h) 
+    {
+        //store the position of the label beginning. The value in this position needs to be set to indicate the length of the upcoming label
+        char *len = p;
+        p++;
+        if (h != argv[1]) 
+        {
+            ++h;
+        }
+        //copy characters from *h to *p until we find a dot or the end of the hostname
+        while(*h && *h != '.') 
+        { 
+            *p++ = *h++;
+        }
+        *len = p - len - 1;
+    }
+    //finish the name section of the question
+    *p++ = 0;
+    //add the question type and question class to the query
+    *p++ = 0x00; *p++ = type; /* QTYPE */
+    *p++ = 0x00; *p++ = 0x01; /* QCLASS */
+    //calculate the query size by comparing p to the query beginning
+    const int query_size = p - query;
+    //transmit the DNS query to the DNS server
+    int bytes_sent = sendto(socket_peer,query, query_size,0,peer_address->ai_addr, peer_address->ai_addrlen);
+    printf("Sent %d bytes.\n", bytes_sent);
+    //For debugging purposes, we can also display the query we sent
+    print_dns_message(query, query_size);
+    //await a DNS response
+    char read[1024];
+    int bytes_received = recvfrom(socket_peer,read, 1024, 0, 0, 0);
+    printf("Received %d bytes.\n", bytes_received);
+    print_dns_message(read, bytes_received);
+    printf("\n");
+    freeaddrinfo(peer_address);
+    CLOSESOCKET(socket_peer);
+    #if defined(_WIN32)
+        WSACleanup();
+    #endif
+    return 0;
+}
+
+
+
