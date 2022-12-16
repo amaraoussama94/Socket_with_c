45f882315e788b18bca0eaf141553c22f0dfdc60
amaraoussama94 <wassa36@yahoo.fr>
2022-12-16 16:27:50 +0100

    all is done

--- ssh_download.c
+++ ssh_download.c
@@ -125,6 +125,74 @@ int main(int argc, char *argv[])
         printf("Authentication successful!\n");
     }
     /***************************************ssh dowload start here*************************************************/
+
+    //prompts  the user for the remote filename
+
+    printf("Remote file to download: ");
+    char filename[128];
+    fgets(filename, sizeof(filename), stdin);
+    filename[strlen(filename)-1] = 0;
+
+    //new SCP session is initialized
+    /*SSH_SCP_READ is passed to ssh_scp_new(). This specifies that we are going 
+    to use the new SCP session for downloading files. The SSH_SCP_WRITE option would 
+    be used to upload files. The libssh library also provides the SSH_SCP_RECURSIVE
+     option to assist with uploading or downloading entire directory trees.*/
+    ssh_scp scp = ssh_scp_new(ssh, SSH_SCP_READ, filename);
+    if (!scp) 
+    {
+        fprintf(stderr, "ssh_scp_new() failed.\n%s\n",ssh_get_error(ssh));
+        return 1;
+    }
+
+    //initialize the SCP channel
+    if (ssh_scp_init(scp) != SSH_OK) 
+    {
+        fprintf(stderr, "ssh_scp_init() failed.\n%s\n",ssh_get_error(ssh));
+        return 1;
+    }
+
+
+    /*begin the file download. This function returns SSH_SCP_REQUEST_NEWFILE 
+    to indicate that the remote host is going to begin sending a new file.*/
+    if (ssh_scp_pull_request(scp) != SSH_SCP_REQUEST_NEWFILE) 
+    {
+        fprintf(stderr, "ssh_scp_pull_request() failed.\n%s\n",ssh_get_error(ssh));
+        return 1;
+    }
+
+
+    //retrieve the remote filename, file size, and permissions.
+    int fsize = ssh_scp_request_get_size(scp);
+    char *fname = strdup(ssh_scp_request_get_filename(scp));
+    int fpermission = ssh_scp_request_get_permissions(scp);
+    printf("Downloading file %s (%d bytes, permissions 0%o\n",fname, fsize, fpermission);
+    free(fname);
+
+    //accepts the new file request
+    ssh_scp_accept_request(scp);
+    //downloads the file
+    if (ssh_scp_read(scp, buffer, fsize) == SSH_ERROR)
+    {
+        fprintf(stderr, "ssh_scp_read() failed.\n%s\n",ssh_get_error(ssh));
+        return 1;
+    }
+
+    printf("Received %s:\n", filename);
+    printf("%.*s\n", fsize, buffer);
+    free(buffer);
+
+    //An additional call to ssh_scp_pull_request() should return SSH_SCP_REQUEST_EOF.
+    //This indicates that we received the entire file from the remote host
+    if (ssh_scp_pull_request(scp) != SSH_SCP_REQUEST_EOF) 
+    {
+        fprintf(stderr, "ssh_scp_pull_request() unexpected.\n%s\n",ssh_get_error(ssh));
+        return 1;
+    }
+
+    ssh_scp_close(scp);
+    ssh_scp_free(scp);
+
     ssh_disconnect(ssh);
     ssh_free(ssh);
     return 0;
