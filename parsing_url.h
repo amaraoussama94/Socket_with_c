88da8ffe3ac3dc1c985d684c7f58ec6e6d12cc74
amaraoussama94 <wassa36@yahoo.fr>
2023-03-26 13:14:02 +0100

    add comment for windows  and linux  user

--- parsing_url.h
+++ parsing_url.h
@@ -3,10 +3,11 @@
 
 #include "macrosheaderwx.h" 
 
-__BEGIN_DECLS
+/* uncomment them for linux only */
+//__BEGIN_DECLS
 
 void parse_url(char *url, char **hostname, char **port, char** path) ;
 
-__END_DECLS
+//__END_DECLS
 
 #endif /* PARSING_URL.h */
